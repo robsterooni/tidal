5bb23dcf679680a18fbb0a2e927a13361a3bc49d
Robin Carlisle <robsterooni@gmail.com>
2022-02-27 17:27:33 +0000

    new device finder

--- scripts/tidal-devices.sh
+++ scripts/tidal-devices.sh
@@ -9,37 +9,29 @@ trim() {
     printf '%s' "$var"
 }
 
-if [[ $EUID -ne 0 ]]; then
-    echo "<2>This script must be run as root" 1>&2
-    exit 1
-fi
 
-configFile=/etc/tidal/config.json
-devicesFile=/var/tidal/devices.json
-
-if [ ! -f $configFile ] ; then
-  echo "<2>configFile(${configFile}) does not exist, leaving" 1>&2
-  exit 2
-fi
-
-rawDevices=$(/usr/ifi/ifi-tidal-release/pa_devs/bin/ifi-pa-devs-get 2> /dev/null)
-
+rawDevices=$(aplay -l)
 devices=""
 while read line; do
-  if [[ $line == device#* ]]; then
-    trimmer=$(cut -d "=" -f2 <<< "$line")
-    devices+="$trimmer"
-    devices+=$'\n'
+  if [[ $line == card* ]]; then
+    card=$(echo "$line" | awk '{print $2}' | grep --only-matching --extended-regexp '[0-9]' )
+    names=$(echo "$line" |  grep -Po "(\[.+?\])" | tr -d "[]")
+    name1=$(sed -n 1p <<< "$names")
+    name2=$(sed -n 2p <<< "$names")
+    if [ "$name1" = "bcm2835 Headphones" ]; then
+      name2="-"
+    fi
+    devices+="${name1}: ${name2} (hw:${card},0)"$'\n'
   fi
 done <<< "$rawDevices"
 
 devices=$(trim "$devices")
 
 if [ -z "$devices" ]; then
-  rm -f $devicesFile
-else
-  echo "$devices" | jo -a > $devicesFile
+    exit 1
 fi
+
+echo "$devices" | jo -a
 
 
 
